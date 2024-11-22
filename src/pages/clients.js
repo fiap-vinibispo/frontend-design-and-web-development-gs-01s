1073e0e8b2e0b51eec5cf3d8382da146609acaa6
vinibispo <vini.bispo015@gmail.com>
2024-11-22 00:56:54 -0300

    Fix a bunch of css, but it's still missing clients page

--- src/pages/clients.js
+++ src/pages/clients.js
@@ -9,4 +9,21 @@ forms.forEach(form => {
     const title = form.children.item(0)
     title.classList.remove("hovered-title")
   })
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    if (!form.checkValidity()) {
+      const inputs = form.querySelectorAll("input:invalid")
+      const textarea = form.querySelector("textarea:invalid")
+      inputs.forEach(i => i.classList.add("error"))
+      textarea.classList.add("error")
+      return
+    }
+
+    if (form.classList.contains("company-form")) {
+      alert("Logo, logo nossa equipe entrará em contato")
+    } else {
+      alert("Agradecemos por se cadastrar na nossa plataforma")
+    }
+  })
 })
